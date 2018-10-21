8f9a0df0dca5a312e9842844ea853cd2054f93ba
Dominic Tarr <dominic.tarr@gmail.com>
2018-10-21 19:18:51 +1300

    test base64 strings with fixed lengths

--- test/index.js
+++ test/index.js
@@ -1,17 +1,28 @@
 var crypto = require('crypto')
 var isCannonicalBase64 = require('../')()
+var isCannonicalBase64_3 = require('../')('','',4)
+var isCannonicalBase64_2 = require('../')('','',3)
+var isCannonicalBase64_1 = require('../')('','',2)
 
 var b = Buffer.alloc(4)
 var b1 = b.slice(1)
 var b2 = b.slice(2)
 for(var i = 0; i < 1<<16; i++) {
   b.writeUInt32BE(i, 0)
+
   if(!isCannonicalBase64.test(b.toString('base64')))
     throw new Error(b.toString('base64')+', ' + i + 'was not cannonical base64')
   if(!isCannonicalBase64.test(b1.toString('base64')))
     throw new Error(b1.toString('base64')+', ' + i + 'was not cannonical base64')
   if(!isCannonicalBase64.test(b2.toString('base64')))
     throw new Error(b2.toString('base64')+', ' + i + 'was not cannonical base64')
+
+  if(!isCannonicalBase64_3.test(b.toString('base64')))
+    throw new Error(b.toString('base64')+', ' + i + 'was not cannonical 4 byte base64')
+  if(!isCannonicalBase64_2.test(b1.toString('base64')))
+    throw new Error(b1.toString('base64')+', ' + i + 'was not cannonical 3 byte base64')
+  if(!isCannonicalBase64_1.test(b2.toString('base64')))
+    throw new Error(b2.toString('base64')+', ' + i + 'was not cannonical 2 byte base64')
 
 }
 
@@ -31,5 +42,7 @@ for(var i = 0; i < 1000; i++) {
 }
 
 console.log('passed')
+
+
 
 
